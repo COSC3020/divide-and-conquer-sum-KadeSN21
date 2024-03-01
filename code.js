7f3e5e0c15e7c385a7b87179cc53aeee5d1999eb
Kade Neville <116524466+KadeSN21@users.noreply.github.com>
2024-02-29 18:35:29 -0700

    Divide and Conquer V1

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
 function divideAndConquerSum(a) {
-    return a;
+  if (a.length == 0) return 0
+  if (a.length == 1) return a[0] 
+  let mid = Math.floor(a.length / 2)
+  let left = divideAndConquerSum(a.slice(0, mid))
+  let right = divideAndConquerSum(a.slice(mid))
+  return left + right
 }
