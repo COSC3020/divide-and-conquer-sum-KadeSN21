b7945c43cc6eb762d33f25268c875836df16926c
Kade Neville <116524466+KadeSN21@users.noreply.github.com>
2024-04-12 01:42:04 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,19 @@
 function divideAndConquerSum(a) {
-  if (a.length == 0) return 0
-  if (a.length == 1) return a[0] 
-  let mid = Math.floor(a.length / 2)
-  let left = divideAndConquerSum(a.slice(0, mid))
-  let right = divideAndConquerSum(a.slice(mid))
-  return left + right
+  const length = a.length;
+  if (length == 0){
+    return 0;
+  }
+  if (length == 1) {
+      return a[0];
+  }
+
+  const oneThird = Math.floor(length / 3);
+  const twoThirds = Math.floor((2 * length) / 3);
+
+  const left = divideAndConquerSum(a.slice(0, oneThird));
+  const middle = divideAndConquerSum(a.slice(oneThird, twoThirds));
+  const right = divideAndConquerSum(a.slice(twoThirds));
+
+  return left + middle + right;
 }
+
